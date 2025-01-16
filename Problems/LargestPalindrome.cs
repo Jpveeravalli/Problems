0e158229162283a71d181480515113a75ef6a5b9
Jpveeravalli <veeravallijayaprakash@gmail.com>
2025-01-16 20:11:31 +0000

    Largest palindrome substring
    This will give largest palindrome in a substring

---
+++ Problems/LargestPalindrome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class LargestPalindrome
+    {
+
+        public string Solution(string s)
+        {
+
+            int stringLength = s.Length;
+            if (stringLength <= 1) return s;
+            int start = 0;
+            int maxLength = 1;
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                for (int j = 0; j <= 1; j++)
+                {
+
+                    int low = i;
+                    int high = i + j;
+
+                    while (low >= 0 && high < stringLength && s[low] == s[high])
+                    {
+
+                        if (high - low + 1 > maxLength)
+                        {
+                            start = low;
+                            maxLength = high - low + 1;
+                        }
+
+                        low--;
+                        high++;
+
+                    }
+                }
+            }
+
+            return s.Substring(start, maxLength);
+        }
+    }
+}
---
+++ UnitTest/LargestPalindromeTest.cs
@@ -0,0 +1,29 @@
+﻿using Problems;
+
+namespace UnitTest
+{
+    public class LargestPalindromeTest
+    {
+        [Fact]
+        public void Test()
+        {
+            LargestPalindrome LP = new LargestPalindrome();
+
+            string result = LP.Solution("abba");
+
+            Assert.Equal("abba", result);
+
+            result = LP.Solution("ba");
+            Assert.Equal("b", result);
+
+            result = LP.Solution("a");
+            Assert.Equal("a", result);
+
+            result = LP.Solution("noonmadamracecar");
+            Assert.Equal("racecar", result);
+
+            result = LP.Solution(string.Empty);
+            Assert.Equal(string.Empty, result);
+        }
+    }
+}
