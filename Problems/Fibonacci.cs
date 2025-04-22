ae1d232916b1a5a9377d7734816408030c27b2f3
Jpveeravalli <veeravallijayaprakash@gmail.com>
2025-04-22 11:58:00 +0100

    DP fabinocci

---
+++ Problems/Fibonacci.cs
@@ -0,0 +1,73 @@
+﻿using Problems;
+
+namespace Problems
+{
+    public class Fibonacci
+    {
+
+        // basic recursive 
+        public int GetFibonacciNumber(int number)
+        {
+            if (number <= 0)
+                return 0;
+            else if (number <= 2)
+                return 1;
+            else
+                return GetFibonacciNumber(number - 1) + GetFibonacciNumber(number - 2);
+
+        }
+
+        public int GetFibonacciNumber_DP_Bottom_UP(int number)
+        {
+            if (number <= 0)
+                return 0;
+            else if (number <= 2)
+                return 1;
+            else
+            {
+                int[] memoized = new int[number + 1];
+                memoized[0] = 0;
+                memoized[1] = 1;
+                for (int i = 2; i <= number; i++)
+                {
+                    memoized[i] = memoized[i - 1] + memoized[i - 2];
+                }
+
+                return memoized[number];
+            }
+
+        }
+
+        public int GetFibonacciNumber_DP_Top_Down(int number)
+        {
+            if (number <= 0)
+                return 0;
+            else if (number <= 2)
+                return 1;
+            else
+            {
+                int[] memoized = new int[number + 1];
+                memoized[0] = 0;
+                memoized[1] = 1;
+                
+                return GetFibonacciNumber_Memoized(number, memoized);
+            }
+
+        }
+
+        private int GetFibonacciNumber_Memoized(int number, int[] memoized)
+        {
+            if (number <= 1)
+                return number;
+
+            if (memoized[number] != 0)
+                return memoized[number];
+            else
+                memoized[number] = GetFibonacciNumber_Memoized(number -1, memoized) + GetFibonacciNumber_Memoized(number - 2, memoized);
+                
+            return memoized[number];
+        }
+
+
+    }
+}
---
+++ UnitTest/FibonacciTest.cs
@@ -0,0 +1,57 @@
+﻿using Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class FibonacciTest
+    {
+        private readonly Fibonacci _fibonacci;
+        public FibonacciTest()
+        {
+            _fibonacci = new Fibonacci();
+        }
+
+        [Fact]
+        public void BasicFibonacciTest()
+        {
+
+            Assert.NotNull(_fibonacci);
+
+            Assert.Equal(0, _fibonacci.GetFibonacciNumber(0));
+            Assert.Equal(1, _fibonacci.GetFibonacciNumber(1));
+            Assert.Equal(1, _fibonacci.GetFibonacciNumber(2));
+            Assert.Equal(89, _fibonacci.GetFibonacciNumber(11));
+
+        }
+
+        [Fact]
+        public void Fibonacci_Bottom_Up_Test()
+        {
+
+            Assert.NotNull(_fibonacci);
+
+            Assert.Equal(0, _fibonacci.GetFibonacciNumber_DP_Bottom_UP(0));
+            Assert.Equal(1, _fibonacci.GetFibonacciNumber_DP_Bottom_UP(1));
+            Assert.Equal(1, _fibonacci.GetFibonacciNumber_DP_Bottom_UP(2));
+            Assert.Equal(89, _fibonacci.GetFibonacciNumber_DP_Bottom_UP(11));
+
+        }
+
+        [Fact]
+        public void Fibonacci_Top_Down_Test()
+        {
+
+            Assert.NotNull(_fibonacci);
+
+            Assert.Equal(0, _fibonacci.GetFibonacciNumber_DP_Top_Down(0));
+            Assert.Equal(1, _fibonacci.GetFibonacciNumber_DP_Top_Down(1));
+            Assert.Equal(1, _fibonacci.GetFibonacciNumber_DP_Top_Down(2));
+            Assert.Equal(89, _fibonacci.GetFibonacciNumber_DP_Top_Down(11));
+
+        }
+    }
+}
